3f6a27fff37f29f2d2a4e1a411073e1778824ece
MBAHSINGO22 <adicahyakriswinar@gmail.com>
2025-06-10 06:06:31 +0700

    selesai schedule seeder

--- database/seeders/ScheduleSeeder.php
+++ database/seeders/ScheduleSeeder.php
@@ -2,16 +2,38 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Schedule;
+use App\Models\Field;
 
 class ScheduleSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
+    public function run()
     {
-        //
+
+        $fields = Field::all();
+
+        foreach ($fields as $field) {
+            $days = ['Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat', 'Sabtu', 'Minggu'];
+
+            foreach ($days as $day) {
+                //jam buka dan tutup (semua lapangan sama: 12:00-23:00)
+                $startHour = 12;
+                $endHour = 23;
+
+                // Slot waktu per jam
+                for ($hour = $startHour; $hour < $endHour; $hour++) {
+                    $startTime = sprintf('%02d:00', $hour);
+                    $endTime = sprintf('%02d:00', $hour + 1);
+
+                    Schedule::create([
+                        'field_id' => $field->id,
+                        'hari' => $day,
+                        'start_time' => $startTime,
+                        'end_time' => $endTime,
+                    ]);
+                }
+            }
+        }
     }
 }
